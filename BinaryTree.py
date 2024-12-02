569dfe76d5695fc4a4b9e5bb7315d5882d481ee5
Atacan <atacanozcihan7@gmail.com>
2024-12-02 16:10:18 +0300

    binary tree commit

---
+++ BinaryTree.py
@@ -0,0 +1,9 @@
+class BTNode:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None        
