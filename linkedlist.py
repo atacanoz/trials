1824898dead2ccb3a1895c249f1ad877b2f2a877
Atacan <atacanozcihan7@gmail.com>
2024-12-02 20:16:11 +0300

    linked list operation declarations

--- linkedlist.py
+++ linkedlist.py
@@ -7,3 +7,38 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def insertAtBeginning(self):
+        pass
+
+    def insertAtEnd(self):
+        pass
+
+    def insertAtIndex(self):
+        pass
+
+    def deleteFromBeginning(self):
+        pass
+
+    def deleteFromEnd(self):
+        pass
+
+    def deleteByValue(self):
+        pass
+
+    def deleteAtIndex(self):
+        pass
+
+    def traverse(self):
+        pass
+
+    def display(self):
+        pass
+
+    def search(self):
+        pass
+
+    def IndexOfValue(self):
+        pass
+
+    
+
