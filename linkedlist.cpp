afcf7dc7be2f21d48dfb12fe1ad5f068221b179a
Atacan <atacanozcihan7@gmail.com>
2024-12-03 15:43:25 +0300

    insert and display

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -22,11 +22,36 @@ public:
         this->head = nullptr;
     }
 
+    void insertAtBegin(int new_data){
+        Node* new_node = new Node(new_data);
+        new_node->next = head;
+        this->head = new_node;
+    }
+
+    void display(){
+        Node* temp = this->head;
+        while (temp != nullptr){
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "nullptr" << endl;
+        delete temp;
+    }
+
 };
 
 
 
 int main(){
+
+    LinkedList list;
+    list.insertAtBegin(10);
+    list.insertAtBegin(20);
+    list.insertAtBegin(30);
+    list.insertAtBegin(40);
+    list.insertAtBegin(50);
+    list.display();
+
 
     return 0;
 }
